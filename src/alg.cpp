d26ecee88acbc375524b1dd5f20763564c4ccfd9
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-03-20 22:30:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,28 +2,24 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int left = 0, right = size, middle = (left + right) / 2, num = 0;
-    while (left != right - 1) {
+    while (*(arr + middle) >= *(arr + left) || *(arr + middle) <= *(arr + right)) {
         middle = (left + right) / 2;
-        if (*(arr + middle) < value)
-            left = middle++;
-        else if (*(arr + middle) > value) {
-            right = middle;
-        }
-        else {
-            num = 1;
-            for (int mid_left = middle - 1; *(arr + mid_left) <= *(arr);
-                 mid_left--) {
-                if (value == *(arr + mid_left))
+        if (*(arr + middle) > value) {
+            middle--;
+        } else if (*(arr + middle) < value) {
+            middle++;
+        } else {
+            num++;
+            int mid_left = middle - 1, mid_right = middle + 1;
+            while (mid_left >= 0) {
+                if (*(arr + mid_left) == value)
                     num++;
-                if (*(arr + mid_left) == *(arr))
-                    break;
+                mid_left--;
             }
-            for (int mid_right = middle + 1; *(arr + mid_right) >= *(arr + size);
-                 mid_right++) {
-                if (value == *(arr + mid_right))
+            while (mid_right < size) {
+                if (*(arr + mid_right) == value)
                     num++;
-                if (*(arr + mid_right) == *(arr + size))
-                    break;
+                mid_right++;
             }
             return num;
         }
